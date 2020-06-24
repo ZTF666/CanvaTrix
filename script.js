187a5104ff51a093de6434dee6bf61afd4cdbaca
ZTF666 <ZTF666@protonmail.ch>
2020-06-24 01:13:12 +0100

    :alien:

--- script.js
+++ script.js
@@ -1,26 +1,25 @@
 function ZTF(selector) {
-    const self = {
-        el: document.querySelector(selector),
-        matrix: (speed, width, height) => {
-            // let s = window.screen
+  const self = {
+    el: document.querySelector(selector),
+    matrix: (speed, width, height) => {
+      // let s = window.screen
 
-            let letters = Array(256).join(1).split('')
-            let draw = function () {
-                self.el.getContext('2d').fillStyle = 'rgba(0,0,0,.05)'
-                self.el.getContext('2d').fillRect(0, 0, width, height)
-                self.el.getContext('2d').fillStyle = '#0F0'
-                letters.map(function (y_pos, index) {
-                    text = String.fromCharCode(3e4 + Math.random() * 50)
-                    x_pos = index * 10
-                    self.el.getContext('2d').fillText(text, x_pos, y_pos)
-                    letters[index] = (y_pos > 758 + Math.random() * 1e4) ? 0 : y_pos + 10
-                })
-            }
-            setInterval(draw, speed)
-        }
-
-    }
-    return self
+      let letters = Array(256).join(1).split("");
+      let draw = function () {
+        self.el.getContext("2d").fillStyle = "rgba(0,0,0,.05)";
+        self.el.getContext("2d").fillRect(0, 0, width, height);
+        self.el.getContext("2d").fillStyle = "#0F0";
+        letters.map(function (y_pos, index) {
+          text = String.fromCharCode(3e4 + Math.random() * 50);
+          x_pos = index * 10;
+          self.el.getContext("2d").fillText(text, x_pos, y_pos);
+          letters[index] = y_pos > 758 + Math.random() * 1e4 ? 0 : y_pos + 10;
+        });
+      };
+      setInterval(draw, speed);
+    },
+  };
+  return self;
 }
 
-module.exports.ZTF = ZTF()
+module.exports.ZTF = ZTF;
